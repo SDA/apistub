4a5c1df9b2f5636d4348fee786460df1e2f6e056
Sasa Djolic <sasa.djolic@sdaworld.com>
2013-05-12 22:21:02 -0700

    Change port number to 8015

--- server.js
+++ server.js
@@ -1,36 +1,28 @@
+// Set the default server configuration.
 var config = {
 	server: {
-		port: 8013
-		}
+		port: 8015
+	}
 };
 
+// Create the web server.
 var http = require('http');
+var server = http.createServer(function (request, response) {
+	if (request.method === 'OPTIONS') {
+		// IE8 does not allow domains to be specified for 'Access-Control-Allow-Origin'.
+		// Only * is allowed.
+		var headers = {};
+		headers["Access-Control-Allow-Origin"] = "*";
+		headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
+		headers["Access-Control-Allow-Credentials"] = true;
+		headers["Access-Control-Max-Age"] = '86400'; // 24 hours
+		headers["Access-Control-Allow-Headers"] = "Accept, Authorization, Content-Type, Origin, X-API-Version, X-HTTP-Method-Override, X-Requested-With";
+		response.writeHead(200, headers);
+		response.end();
+		return;
+	}
 
-  
-  
-   // Create the web server.
-    var server = http.createServer(function (request, response) {
-      
-      
-        if (request.method === 'OPTIONS') {
-
-            // IE8 does not allow domains to be specified for 'Access-Control-Allow-Origin'.
-            // Only * is allowed.
-
-            var headers = {};
-            headers["Access-Control-Allow-Origin"] = "*";
-            headers["Access-Control-Allow-Methods"] = "POST, GET, PUT, DELETE, OPTIONS";
-            headers["Access-Control-Allow-Credentials"] = true;
-            headers["Access-Control-Max-Age"] = '86400'; // 24 hours
-            headers["Access-Control-Allow-Headers"] = "Accept, Authorization, Content-Type, Origin, X-API-Version, X-HTTP-Method-Override, X-Requested-With";
-            response.writeHead(200, headers);
-            response.end();
-
-            return;
-        }
-      
-          response.writeHead(500, {"Content-Type": "text/plain"});
-		  response.write("system error");
-		  response.end();
-	}).listen(config.server.port);
-	  
+	response.writeHead(500, {"Content-Type": "text/plain"});
+	response.write("system error");
+	response.end();
+}).listen(config.server.port);
